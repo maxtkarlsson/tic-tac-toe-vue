3e361759169d6415215354af5c4cd4548b76e598
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-04-13 12:41:04 +0200

    (feat): Add GameBoard

--- src/components/AppGame.vue
+++ src/components/AppGame.vue
@@ -11,9 +11,16 @@ const state = ref<GameState>({
   isGamerOver: false,
 });
 
+const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+};
+
 const addPlayer = (name: string) => {
   const playerToAdd = new Player(0, name, 0);
   const playerlist = state.value.players;
+  let currentPlayer = state.value.currentPlayer;
 
   if (playerlist.length < 3) {
     if (playerlist.length === 0) {
@@ -22,14 +29,43 @@ const addPlayer = (name: string) => {
     } else if (playerlist.length === 1) {
       playerToAdd.id = 2;
       playerlist.push(playerToAdd);
+      currentPlayer = getRandomInt(1, 3);
+      console.log(currentPlayer);
     }
   }
+
   console.log(state.value);
+};
+
+const placeGamePiece = (clickedSquare: number) => {
+  console.log("Du klickade på ruta: ", clickedSquare);
 };
 </script>
 
 <template>
   <AppRegistration @add-player="addPlayer" />
+  <div class="game">
+    <div
+      class="game__square"
+      v-for="(square, index) in state.gameboard"
+      @click="placeGamePiece(index)"
+    >
+      {{ square }}
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.game {
+  border: 2px solid brown;
+  display: flex;
+  flex-wrap: wrap;
+  width: 156px;
+  height: 156px;
+}
+.game__square {
+  border: 1px solid white;
+  width: 50px;
+  height: 50px;
+}
+</style>
---
+++ src/components/AppGameBoard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { GameState } from "../models/GameState";
+
+interface IAppGameBoardProps {
+  gameState: GameState;
+}
+
+const props = defineProps<IAppGameBoardProps>();
+</script>
+
+<template>
+  <div class="game">
+    <div class="game__square" v-for="square in gameState.gameboard"></div>
+  </div>
+</template>
+
+<style scoped></style>
